b4a79f88989963ad21cbd4aa113837eda62e6b9f
gurami <guramidgebuadze@gmail.com>
2022-12-07 18:01:47 +0400

    finished refactoring

--- getFilmData.js
+++ getFilmData.js
@@ -7,21 +7,16 @@ export function filmData(searchedMovie) {
 
   getFilmData(searchedMovie.value)
     .then((data) => {
-      if (data.Response === "True") {
-        let ageOfMovie = new Date().getFullYear() - data.Year;
-        let actors = data.Actors;
-        let country = data.Country;
-        movieInfo(ageOfMovie, actors, country);
-      }
+      let ageOfMovie = new Date().getFullYear() - data.Year;
+      let actors = data.Actors;
+      let country = data.Country;
+      movieInfo(ageOfMovie, actors, country);
     })
     .catch(() => console.log("error occured"));
   searchedMovie.value = "";
 }
 
 // Multiple movies search info button
-
-let population = 0;
-
 export function multipleFilmsData(
   multipleMoviesInput1,
   multipleMoviesInput2,
@@ -35,17 +30,15 @@ export function multipleFilmsData(
     getFilmData(multipleMoviesInput2.value),
     getFilmData(multipleMoviesInput3.value),
   ]).then((result) => {
-    let countries = "";
+    let countries = [];
     let duration = 0;
     result.forEach((data) => {
       duration += +data.Runtime.split(" ")[0];
       // Defend code to repeat countries movies are made in
       let newCountries = data.Country.split(",");
-      console.log(newCountries);
       newCountries.forEach((country) => {
-        if (!countries.includes(country)) {
-          countries += data.Country + ", ";
-        }
+        countries.push(country);
+        countries = [...new Set(countries)];
       });
     });
 
